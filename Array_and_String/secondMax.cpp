b27f9201f1e4ce99ea99c4fd5f37ece6136174f4
Swairik <swairikdey1001@gmail.com>
2022-04-27 19:40:52 +0530

    added second max element of array

--- Array_and_String/secondMax.cpp
+++ Array_and_String/secondMax.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
-    cout << "hello world";
+    int n;
+    cout << "Enter num of elements (n>=2): ";
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int mx1 = a[0], mx2 = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(a[i] >= mx1) {
+            mx2 = mx1;
+            mx1 = a[i];
+        }
+    }
+    cout << "max = " << mx1 << " " << "max2 = " << mx2 << endl;
 }
